3e1b7654536906742baaf04d13c2e2ec6aaf9a2c
gaori <monooi@naver.com>
2023-12-14 23:56:17 +0900

    Feat: form spinner 추가

---
+++ apps/next-advanced/app/components/Spinner.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Spinner = () => {
+  return (
+    <div className='flex space-x-1 justify-center items-center h-screen dark:invert'>
+      <span className='sr-only'>Loading...</span>
+      <div className='h-2 w-2 bg-white rounded-full animate-bounce [animation-delay:-0.3s]'></div>
+      <div className='h-2 w-2 bg-white rounded-full animate-bounce [animation-delay:-0.15s]'></div>
+      <div className='h-2 w-2 bg-white rounded-full animate-bounce'></div>
+    </div>
+  );
+};
+
+export default Spinner;
