9f279f4429d9bd7c29b29128b50f094cf5af9187
gaori <monooi@naver.com>
2023-12-17 22:10:43 +0900

    Feat: 이슈 디테일 페이지 제작

---
+++ apps/next-advanced/app/issues/[id]/page.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import prisma from '@/prisma/client';
+import { notFound } from 'next/navigation';
+
+interface Props {
+  params: { id: string };
+}
+
+const IssueDetailPage = async ({ params }: Props) => {
+  const issue = await prisma.issue.findUnique({
+    where: { id: parseInt(params.id) },
+  });
+
+  if (!issue) notFound();
+
+  return (
+    <>
+      <p>{issue.title}</p>
+      <p>{issue.description}</p>
+      <p>{issue.status}</p>
+      <p>{issue.createdAt.toDateString()}</p>
+    </>
+  );
+};
+
+export default IssueDetailPage;
